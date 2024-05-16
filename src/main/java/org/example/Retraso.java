4d8bce361ec7af2a9e1a0a9067ad1212031fe335
Roberto <pantostado1601@gmail.com>
2024-05-15 21:44:13 -0400

    Added: Clase Retraso  //  Se agrego clase reunion y se importo java.time.instant a la clase

--- src/main/java/org/example/Retraso.java
+++ src/main/java/org/example/Retraso.java
@@ -1,4 +1,26 @@
 package org.example;
+import java.time.Instant;
 
 public class Retraso {
+    private Instant hora;
+
+    public Retraso(Instant hora) {
+        this.hora = hora;
+    }
+
+    // Getter y Setter
+    public Instant getHora() {
+        return hora;
+    }
+
+    public void setHora(Instant hora) {
+        this.hora = hora;
+    }
+
+    @Override
+    public String toString() {
+        return "Retraso{" +
+                "hora=" + hora +
+                '}';
+    }
 }
