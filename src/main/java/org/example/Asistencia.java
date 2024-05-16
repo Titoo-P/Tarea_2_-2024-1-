fb6a2743db937ab4d733c35cc0279b2463781e9e
Martin G <martingonzalez2022@udec.cl>
2024-05-15 21:03:22 -0400

    Added: Clase Asistencia // Se agrega funcionamiento completo de la clase

--- src/main/java/org/example/Asistencia.java
+++ src/main/java/org/example/Asistencia.java
@@ -1,4 +1,18 @@
 package org.example;
 
 public class Asistencia {
+    private final Empleado empleado;
+
+    public Asistencia(Empleado empleado) {
+        this.empleado = empleado;
+    } // Constructor que inicializa al empleado que asiste a la reunion
+
+    public Empleado getEmpleado() {
+        return empleado;
+    } // Metodo que devuelve el empleado que asiste a la reunion
+
+    @Override
+    public String toString() {
+        return "Asistencia{" + "empleado=" + empleado + '}';
+    } // Metodo que devuelve el empleado que asiste a la reunion
 }
