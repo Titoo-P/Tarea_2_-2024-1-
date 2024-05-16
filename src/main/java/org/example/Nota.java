b9d20d5fe3385cc4ea4e88281d7e8dcbedb2b4ea
Roberto <pantostado1601@gmail.com>
2024-05-15 21:53:13 -0400

    Added: Clase Nota //  Se agrega clase Nota

--- src/main/java/org/example/Nota.java
+++ src/main/java/org/example/Nota.java
@@ -1,4 +1,27 @@
 package org.example;
 
 public class Nota {
+
+    private String contenido;
+
+    public Nota(String contenido) {
+        this.contenido = contenido;
+    }
+
+    // Getter y Setter
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+    // Método para representar el objeto como String
+    @Override
+    public String toString() {
+        return "Nota{" +
+                "contenido='" + contenido + '\'' +
+                '}';
+    }
 }
