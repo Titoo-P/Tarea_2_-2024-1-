af9b24fa63b3abce30d2b55871d9c8aab34edbd5
Roberto <pantostado1601@gmail.com>
2024-05-16 08:09:53 -0400

    added: javadoc clase invitacion // Implementacion documentacion de forma basica para invitacion

--- src/main/java/org/example/Invitacion.java
+++ src/main/java/org/example/Invitacion.java
@@ -2,31 +2,59 @@ package org.example;
 
 import java.time.Instant;
 
+/**
+ * La clase Invitacion representa una invitación a una reunión para un invitado específico.
+ */
 public class Invitacion {
     private final Instant hora;
     private final Invitable invitado;
     private final Reunion reunion;
 
-
+    /**
+     * Constructor que inicializa una invitación para una reunión y un invitado específicos.
+     *
+     * @param reunion  La reunión a la que se invita.
+     * @param invitado El objeto que implementa la interfaz Invitable y que representa al invitado.
+     */
     public Invitacion(Reunion reunion, Invitable invitado) {
         this.reunion = reunion;
         this.invitado = invitado;
         this.hora = reunion.getHoraPrevista();
     }
 
-    // Getter y Setter
+    /**
+     * Devuelve la hora de la invitación.
+     *
+     * @return La hora de la invitación.
+     */
     public Instant getHora() {
         return hora;
     }
 
-    public Invitable getInvitado(){
+    /**
+     * Devuelve el invitado de la invitación.
+     *
+     * @return El objeto que implementa la interfaz Invitable y que representa al invitado.
+     */
+    public Invitable getInvitado() {
         return invitado;
     }
 
-    public Reunion getReunion(){
+    /**
+     * Devuelve la reunión a la que se refiere la invitación.
+     *
+     * @return La reunión a la que se refiere la invitación.
+     */
+    public Reunion getReunion() {
         return reunion;
     }
- //
+
+    /**
+     * Devuelve una representación en formato de cadena de la invitación,
+     * incluyendo la hora de la invitación.
+     *
+     * @return Una cadena que representa la invitación.
+     */
     @Override
     public String toString() {
         return "Invitacion{" +
@@ -34,3 +62,4 @@ public class Invitacion {
                 '}';
     }
 }
+
