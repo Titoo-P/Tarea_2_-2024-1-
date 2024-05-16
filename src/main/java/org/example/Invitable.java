aec37a81a5ed0056a431c53243d5ca903024fc37
Roberto <pantostado1601@gmail.com>
2024-05-16 08:06:37 -0400

    added: javadoc clase invitable // Documentacion basica para clase invitable

--- src/main/java/org/example/Invitable.java
+++ src/main/java/org/example/Invitable.java
@@ -1,9 +1,14 @@
 package org.example;
 
-import java.time.Instant;
-
+/**
+ * La interfaz Invitable define un método para invitar a la clase que la implementa a una reunión.
+ */
 public interface Invitable {
 
+    /**
+     * Invita a la clase que implementa la interfaz a una reunión.
+     *
+     * @param reunion La reunión a la que se invita a la clase.
+     */
     public void invitar(Reunion reunion);
-    // Metodo que invita a la clase que implementa la interfaz a una reunion
 }
