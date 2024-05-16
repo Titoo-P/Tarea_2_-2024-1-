f6ffd5b0fb288f3fc96c826331ab00458b3159cd
Roberto <pantostado1601@gmail.com>
2024-05-15 22:55:38 -0400

    added: Clase Invitacion // se agrega invitacion de forma basica

--- src/main/java/org/example/Invitacion.java
+++ src/main/java/org/example/Invitacion.java
@@ -1,4 +1,27 @@
 package org.example;
 
+import java.time.Instant;
+
 public class Invitacion {
+    private Instant hora;
+
+    public Invitacion(Instant hora) {
+        this.hora = hora;
+    }
+
+    // Getter y Setter
+    public Instant getHora() {
+        return hora;
+    }
+
+    public void setHora(Instant hora) {
+        this.hora = hora;
+    }
+
+    @Override
+    public String toString() {
+        return "Invitacion{" +
+                "hora=" + hora +
+                '}';
+    }
 }
