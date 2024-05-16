2e3a1e31a9710e358f95b9eef0055dcc79bac06d
Martin G <martingonzalez2022@udec.cl>
2024-05-16 02:48:14 -0400

    Added: Clase tipoReunion // Implementacion general de la clase, agregando su respectivo identificador.

--- src/main/java/org/example/tipoReunion.java
+++ src/main/java/org/example/tipoReunion.java
@@ -1,7 +1,18 @@
 package org.example;
 
 public enum tipoReunion {
-    TECNICA,
-    MARKETING,
-    OTRO;
+    TECNICA("Tecnica"),
+    MARKETING("Marketing"),
+    OTRO("Otro");
+
+    public final String tipo;
+
+    tipoReunion(String tipo){
+        this.tipo = tipo;
+    }
+
+    @Override
+    public String toString(){
+        return tipo;
+    }
 }
