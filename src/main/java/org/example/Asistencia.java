2c3e4ed97ff750fa3bafcaceba182ac51751460b
Roberto <pantostado1601@gmail.com>
2024-05-16 07:59:01 -0400

    added: javadoc clase Asistencia

--- src/main/java/org/example/Asistencia.java
+++ src/main/java/org/example/Asistencia.java
@@ -1,18 +1,38 @@
 package org.example;
 
+/**
+ * La clase Asistencia representa la asistencia de un empleado a una reunión.
+ * Contiene la información del empleado que asiste.
+ */
 public class Asistencia {
     private final Empleado empleado;
 
+    /**
+     * Constructor que inicializa al empleado que asiste a la reunión.
+     *
+     * @param empleado El empleado que asiste a la reunión.
+     */
     public Asistencia(Empleado empleado) {
         this.empleado = empleado;
-    } // Constructor que inicializa al empleado que asiste a la reunion
+    }
 
+    /**
+     * Devuelve el empleado que asiste a la reunión.
+     *
+     * @return El empleado que asiste a la reunión.
+     */
     public Empleado getEmpleado() {
         return empleado;
-    } // Metodo que devuelve el empleado que asiste a la reunion
+    }
 
+    /**
+     * Devuelve una representación en formato de cadena de la asistencia,
+     * incluyendo la información del empleado.
+     *
+     * @return Una cadena que representa la asistencia.
+     */
     @Override
     public String toString() {
         return "Asistencia{" + "empleado=" + empleado + '}';
-    } // Metodo que devuelve el empleado que asiste a la reunion
+    }
 }
