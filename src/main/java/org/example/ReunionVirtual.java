6907f8c981d6c3ab235e9f5a2f47f0221829c7e2
Martin G <martingonzalez2022@udec.cl>
2024-05-16 02:47:19 -0400

    Added: Clase ReunionVirtual // Implementacion general de la clase.

--- src/main/java/org/example/ReunionVirtual.java
+++ src/main/java/org/example/ReunionVirtual.java
@@ -1,4 +1,20 @@
 package org.example;
 
-public class ReunionVirtual {
+import java.time.Instant;
+import java.time.Duration;
+import java.util.Date;
+
+public class ReunionVirtual extends Reunion{
+
+    private String enlace;
+
+    public ReunionVirtual(Empleado empleado, tipoReunion tipo, Date fecha, Duration duracionPrevista, Instant horaPrevista, String enlace){
+        super(empleado, tipo, fecha, horaPrevista, duracionPrevista);
+        this.enlace = enlace;
+    }
+
+    @Override
+    public String toString(){
+        return super.toString() + "de tipo Virtual";
+    }
 }
