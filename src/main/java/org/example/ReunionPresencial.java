103e549f95cf6fb488b069083ddbbd30e3bf0ffc
Martin G <martingonzalez2022@udec.cl>
2024-05-16 02:34:20 -0400

    Added: Clase ReunionPresencial // Implementacion general de la clase

--- src/main/java/org/example/ReunionPresencial.java
+++ src/main/java/org/example/ReunionPresencial.java
@@ -1,4 +1,24 @@
 package org.example;
 
-public class ReunionPresencial {
+import java.time.Instant;
+import java.time.Duration;
+import java.util.Date;
+
+public class ReunionPresencial extends Reunion{
+
+    private String sala;
+
+    public ReunionPresencial(Empleado empleado, tipoReunion tipo, Date fecha, Duration duracionPrevista, String sala, Instant horaPrevista){
+        super(empleado, tipo, fecha, horaPrevista, duracionPrevista);
+        this.sala = sala;
+    }
+
+    public String getSala(){
+        return sala;
+    }
+
+    @Override
+    public String toString(){
+        return  super.toString() + "en la sala";
+    }
 }
