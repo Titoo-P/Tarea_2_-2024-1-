281a1a21692e36809c89af627c0de6f59c0d2490
Roberto <pantostado1601@gmail.com>
2024-05-17 11:12:22 -0400

    added: Clase main // Clase main ahora crea un archivo .txt de la reunion

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.time.Duration;
 import java.util.List;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
@@ -78,6 +80,31 @@ public class Main {
         System.out.println("Empleados atrasados: ");
         for (Asistencia a : reunion.obtenerRetrasos()) {
             System.out.println("- " + a.getEmpleado().getNombre());
+        }
+
+        //Crear el informe en un .txt
+
+        try (FileWriter writer = new FileWriter("informe_reunion.txt")) {
+            writer.write("Organizador de la reunión: " + reunion.getOrganizador().getNombre() + "\n");
+            writer.write("Hora de inicio: " + reunion.getHorarioInicio() + "\n");
+            writer.write("Hora de finalización: " + reunion.getHorarioFinal() + "\n");
+            writer.write("Duración real: " + reunion.tiempoReal() + " segundos\n");
+            writer.write("Asistencias totales: " + reunion.AsistenciaTotal() + "\n");
+            writer.write("Porcentaje de asistencia: " + reunion.AsistenciaPorcentaje() + "%\n");
+            writer.write("Notas de la reunión: \n");
+            for (Nota n : reunion.getNotas()) {
+                writer.write("- " + n.getContenido() + "\n");
+            }
+            writer.write("Empleados ausentes: \n");
+            for (Empleado e : reunion.obtenerAusencias()) {
+                writer.write("- " + e.getNombre() + "\n");
+            }
+            writer.write("Empleados atrasados: \n");
+            for (Asistencia a : reunion.obtenerRetrasos()) {
+                writer.write("- " + a.getEmpleado().getNombre() + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
 
