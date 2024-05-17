e58c896bb5f9276759b415f2d8314c9791b98ecb
Martin G <martingonzalez2022@udec.cl>
2024-05-16 23:07:35 -0400

    fix: TestAsistencia // agrege instant.now para que tome la hora de la clase

--- src/test/java/TestAsistencia.java
+++ src/test/java/TestAsistencia.java
@@ -3,12 +3,15 @@ import org.example.*;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class TestAsistencia{
+import java.time.Instant;
+
+class TestAsistencia {
 
     @Test
-    void testGetEmpleado(){
+    void testGetEmpleado() {
+
         Empleado empleado = new Empleado("1", "Guzman", "Pedro", "PeGuz@example.com");
-        Asistencia asistencia = new Asistencia(empleado);
+        Asistencia asistencia = new Asistencia(empleado, Instant.now());
 
         assertEquals(empleado, asistencia.getEmpleado());
     }
